1d4e276fa90227b2fea74b2b06cd089892bf2c44
David Dudson <davidjohndudson@gmail.com>
2017-12-11 15:11:06 +1300

    Add nested expansion test

--- scalagen/shared/src/test/scala/org/scalameta/scalagen/TestExtensions.scala
+++ scalagen/shared/src/test/scala/org/scalameta/scalagen/TestExtensions.scala
@@ -2,8 +2,6 @@ package org.scalameta.scalagen
 
 import scala.meta._
 import scala.meta.contrib._
-import org.scalameta.scalagen._
-import org.scalameta.scalagen.generators._
 import org.scalatest._
 
 class TestExtensions extends FunSuite {
@@ -17,6 +15,25 @@ class TestExtensions extends FunSuite {
 
     assert(clazz.extract[Stat].size === 0)
     assert(out.extract[Stat].size === 1)
+    assert(!clazz.isEqual(out))
+  }
+
+  test("Nested Expansion works") {
+    val inner: Defn.Class = q"@StructuralToString case class Bar(y: Int)"
+
+    val clazz: Defn.Class =
+      q"@StructuralToString case class Foo(x: Int, y: Int) { $inner }"
+    val generator = new StructuralToString()
+    val runner = Runner(clazz, Set(generator))
+
+    val out: Defn.Class = runner.transform
+
+    assert(clazz.extract[Stat].size === 1)
+    assert(out.extract[Stat].size === 2)
+    val outInner = out.extract[Stat].head.asInstanceOf[Defn.Class]
+
+    assert(outInner.extract[Stat].size === 1)
+    assert(outInner.withStats(Nil) isEqual inner)
     assert(!clazz.isEqual(out))
   }
 
