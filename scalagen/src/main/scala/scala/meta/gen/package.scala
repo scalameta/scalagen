3ab556bf581691f05af8a7e03354e9f57c7ab1c8
David Dudson <davidjohndudson@gmail.com>
2017-12-29 16:17:00 +1300

    Add basic logging for generator authors

--- scalagen/src/main/scala/scala/meta/gen/package.scala
+++ scalagen/src/main/scala/scala/meta/gen/package.scala
@@ -3,4 +3,4 @@ package scala.meta
 import scala.meta.gen.implicits.implicits
 import scala.meta.gen.instances.instances
 
-package object gen extends implicits with instances
+package object gen extends implicits with instances with ScalagenErrorHandler
