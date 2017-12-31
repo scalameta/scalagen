a66a34012d44518985360c0e86322a4785b9a894
David Dudson <davidjohndudson@gmail.com>
2017-12-31 20:53:06 +1300

    WIP

--- scalagen/src/main/scala/scala/meta/gen/implicits/implicits.scala
+++ scalagen/src/main/scala/scala/meta/gen/implicits/implicits.scala
@@ -1,3 +1,3 @@
 package scala.meta.gen.implicits
 
-trait implicits extends Traversal with Owner with FindCompanion
+trait implicits extends Traversal with Owner with FindCompanion with ExtractAndReplaceExtras
